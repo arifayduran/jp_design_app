180e963b9e567b9479582a6c02d9574bf3fa1018
Arif Ayduran <167970829+Arifnator@users.noreply.github.com>
2024-07-17 05:25:14 +0300

    not

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,6 @@ import 'package:jp_design_app/src/config/my_theme_data.dart';
 import 'package:jp_design_app/src/core/start_screen.dart';
 import 'package:scaled_app/scaled_app.dart';
 
-// stoke olayi tam & radius + .filled dene micha + elevated ters cefvir icine yap?
-
-// naisl yaptim lan liste ffiltresini??
-
-// iphone da calistir
-
-// recommend card schatten
-// all categorien auwsähblar
-// description yoksa subtitble alsin
-// baska simulator
-// animasyon yap splash?
-// scale meseleelri + modal botton sheet?
-
-// SF BOLD??? --> salty fln
-// eski yaptiklarimla kiyasla
-// inner shadow hallet
-// button stroke blur
-
 void main() async {
   await Future.delayed(const Duration(seconds: 1));
   FlutterNativeSplash.remove();
